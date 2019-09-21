bf1483aeb13b88f064008b72488d7261a0063123
Tarun9005 <shekhertarun@gmail.com>
2019-09-21 14:27:30 +0530

    first

---
+++ home.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+if(!isset($_SESSION['username'])){
+header('location: login.php');
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+	<h2> Welcome <?php echo $_SESSION['username']; ?> </h2>
+	<a href="logout.php">Logout</a>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+	<div>
+		<form action="validation.php" method="post">
+			<input type="text" name="user">
+			<br>
+			<br>
+			<input type="text" name="password">
+			<br>
+			<br>
+			<button type="submit">Login</button>
+		</form>
+		<br>
+		<br>
+	</div>
+
+	<div>
+
+		<form action="registration.php" method="post">
+			<input type="text" name="user">
+			<br>
+			<br>
+			<input type="text" name="password">
+			<br>
+			<br>
+			<button type="submit">Signup</button>
+		</form>
+	</div>
